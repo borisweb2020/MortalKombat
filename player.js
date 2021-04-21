050b0b332587d61a4b0f589726b565e0e90d6c54
borisweb2020 <49887284+borisweb2020@users.noreply.github.com>
2021-04-21 12:29:55 +0700

    stage-7

--- player.js
+++ player.js
@@ -1,18 +1,78 @@
-export const newPlayer1 = {
-	name: 'Mister',
-	weapon: ['JS', 'CSS', 'HTML'],
-	hp: {
-		current: 80,
-		total: 100,
+import {getRandom} from './utils.js';
+import {currentTime} from './utils.js';
+
+import * as variables from './variables.js';
+
+const {$chat, logs} = variables;
+
+
+export const player1 = {
+	player: 1,
+	name: 'Scorpion',
+	hp: 100,
+	img: 'http://reactmarathon-api.herokuapp.com/assets/scorpion.gif',
+	weapon: ['sword', 'harpoon', 'star'],
+	changeHP,
+	elHP,
+	renderHP,
+	generateLog,
+	attack: function(){
+		console.log('Fight...')
 	}
 }
 
-const newPlayer2 = {
-	name: 'Sir',
-	weapon: ['jQuery', 'CSS-3', 'HTML-5'],
-	hp: {
-		current: 800,
-		total: 1000,
+
+
+export const player2 = {
+	player: 2,
+	name: 'Kitana',
+	hp: 100,
+	img: 'http://reactmarathon-api.herokuapp.com/assets/kitana.gif',
+	weapon: ['sword', 'harpoon', 'star'],
+	changeHP,
+	elHP,
+	renderHP,
+	generateLog,
+	attack: function(){
+		console.log('Fight...')
 	}
 }
+
+function changeHP(arg){
+	this.hp -= arg;
+
+	if(this.hp <= 0){
+		this.hp = 0;
+	}
+}
+
+function elHP(){
+	const $playerLife = document.querySelector('.player' + this.player + ' .life');
+	return $playerLife;
+}
+
+function renderHP(){
+	const $thisPlayerLife       = this.elHP();
+	$thisPlayerLife.style.width = this.hp + '%';
+}
+
+function generateLog(type, damage, enemy){
+	let i = getRandom(logs[type].length - 1);
+	let text;
+	let el;
+
+	switch(type){
+		case 'hit':
+			text = logs['hit'][i].replace('[playerDefence]', this.name).replace('[playerKick]', enemy.name);
+			el   = `<p> ${currentTime()} ${text} -${damage} [${this.hp}/100]</p>`;
+			break;
+		case 'defence':
+			text = logs['defence'][i].replace('[playerKick]', enemy.name).replace('[playerDefence]', this.name);
+			el   = `<p> ${currentTime()} ${text} </p>`;
+			break;
+	}
+
+	$chat.insertAdjacentHTML('afterbegin', el);
+}
+
 
--- utils.js
+++ utils.js
@@ -1,6 +1,44 @@
-function getRandom(maxValue){
+export function currentTime(){
+	const date  = new Date();
+	let hours   = (date.getHours()).toString();
+	let minutes = (date.getMinutes()).toString();
+
+	if(hours.length < 2){
+		hours = '0' + hours;
+	}
+
+	if(minutes.length < 2){
+		minutes = '0' + minutes;
+	}
+
+	return hours + ':' + minutes;
+}
+
+
+export function getRandom(maxValue){
 	const result = Math.ceil(Math.random() * maxValue);
 	return result ;
 }
 
-export default getRandom;
+
+export function playerWin(name){
+	const $winTitle = createElement('div', 'loseTitle');
+
+	if(name){
+		$winTitle.textContent = name + ' wins';
+	} else {
+		$winTitle.textContent = 'draw';
+	}
+
+	return $winTitle;
+}
+
+export function createElement(tag, className){
+	const $tag = document.createElement(tag);
+
+	if(className){
+		$tag.classList.add(className);
+	}
+
+	return $tag;
+}
