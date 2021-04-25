f19a469e4cfbc886bd402b346f2fe7f0339f6c19
borisweb2020 <49887284+borisweb2020@users.noreply.github.com>
2021-04-25 13:30:11 +0700

    Stage-8

--- player.js
+++ player.js
@@ -5,74 +5,139 @@ import * as variables from './variables.js';
 
 const {$chat, logs} = variables;
 
+class Player {
+	constructor(props){
+		this.player = props.player;
+		this.name   = props.name;
+		this.hp     = props.hp;
+		this.img    = props.img;
+	}
 
-export const player1 = {
+	changeHP = arg => {
+		this.hp -= arg;
+
+		if(this.hp <= 0){
+			this.hp = 0;
+		}
+	}
+
+	elHP = () => {
+		const $playerLife = document.querySelector('.player' + this.player + ' .life');
+		return $playerLife;
+	}
+
+	renderHP = () => {
+		const $thisPlayerLife       = this.elHP();
+		$thisPlayerLife.style.width = this.hp + '%';
+	}
+
+	generateLog = (type, damage, enemy) => {
+		let i = getRandom(logs[type].length - 1);
+		let text;
+		let el;
+
+		switch(type){
+			case 'hit':
+				text = logs['hit'][i].replace('[playerDefence]', this.name).replace('[playerKick]', enemy.name);
+				el   = `<p> ${currentTime()} ${text} -${damage} [${this.hp}/100]</p>`;
+				break;
+			case 'defence':
+				text = logs['defence'][i].replace('[playerKick]', enemy.name).replace('[playerDefence]', this.name);
+				el   = `<p> ${currentTime()} ${text} </p>`;
+				break;
+		}
+
+		$chat.insertAdjacentHTML('afterbegin', el);
+	}
+
+	attack = () => {
+		console.log('Fight ...');
+	}
+}
+
+
+export const player1 = new Player({
 	player: 1,
 	name: 'Scorpion',
 	hp: 100,
 	img: 'http://reactmarathon-api.herokuapp.com/assets/scorpion.gif',
-	weapon: ['sword', 'harpoon', 'star'],
-	changeHP,
-	elHP,
-	renderHP,
-	generateLog,
-	attack: function(){
-		console.log('Fight...')
-	}
-}
+});
 
-
-
-export const player2 = {
+export const player2 = new Player({
 	player: 2,
 	name: 'Kitana',
 	hp: 100,
 	img: 'http://reactmarathon-api.herokuapp.com/assets/kitana.gif',
-	weapon: ['sword', 'harpoon', 'star'],
-	changeHP,
-	elHP,
-	renderHP,
-	generateLog,
-	attack: function(){
-		console.log('Fight...')
-	}
-}
+});
 
-function changeHP(arg){
-	this.hp -= arg;
 
-	if(this.hp <= 0){
-		this.hp = 0;
-	}
-}
+// export const player1 = {
+// 	player: 1,
+// 	name: 'Scorpion',
+// 	hp: 100,
+// 	img: 'http://reactmarathon-api.herokuapp.com/assets/scorpion.gif',
+// 	weapon: ['sword', 'harpoon', 'star'],
+// 	changeHP,
+// 	elHP,
+// 	renderHP,
+// 	generateLog,
+// 	attack: function(){
+// 		console.log('Fight...')
+// 	}
+// }
 
-function elHP(){
-	const $playerLife = document.querySelector('.player' + this.player + ' .life');
-	return $playerLife;
-}
 
-function renderHP(){
-	const $thisPlayerLife       = this.elHP();
-	$thisPlayerLife.style.width = this.hp + '%';
-}
 
-function generateLog(type, damage, enemy){
-	let i = getRandom(logs[type].length - 1);
-	let text;
-	let el;
+// export const player2 = {
+// 	player: 2,
+// 	name: 'Kitana',
+// 	hp: 100,
+// 	img: 'http://reactmarathon-api.herokuapp.com/assets/kitana.gif',
+// 	weapon: ['sword', 'harpoon', 'star'],
+// 	changeHP,
+// 	elHP,
+// 	renderHP,
+// 	generateLog,
+// 	attack: function(){
+// 		console.log('Fight...')
+// 	}
+// }
 
-	switch(type){
-		case 'hit':
-			text = logs['hit'][i].replace('[playerDefence]', this.name).replace('[playerKick]', enemy.name);
-			el   = `<p> ${currentTime()} ${text} -${damage} [${this.hp}/100]</p>`;
-			break;
-		case 'defence':
-			text = logs['defence'][i].replace('[playerKick]', enemy.name).replace('[playerDefence]', this.name);
-			el   = `<p> ${currentTime()} ${text} </p>`;
-			break;
-	}
+// function changeHP(arg){
+// 	this.hp -= arg;
 
-	$chat.insertAdjacentHTML('afterbegin', el);
-}
+// 	if(this.hp <= 0){
+// 		this.hp = 0;
+// 	}
+// }
+
+// function elHP(){
+// 	const $playerLife = document.querySelector('.player' + this.player + ' .life');
+// 	return $playerLife;
+// }
+
+// function renderHP(){
+// 	const $thisPlayerLife       = this.elHP();
+// 	$thisPlayerLife.style.width = this.hp + '%';
+// }
+
+// function generateLog(type, damage, enemy){
+// 	let i = getRandom(logs[type].length - 1);
+// 	let text;
+// 	let el;
+
+// 	switch(type){
+// 		case 'hit':
+// 			text = logs['hit'][i].replace('[playerDefence]', this.name).replace('[playerKick]', enemy.name);
+// 			el   = `<p> ${currentTime()} ${text} -${damage} [${this.hp}/100]</p>`;
+// 			break;
+// 		case 'defence':
+// 			text = logs['defence'][i].replace('[playerKick]', enemy.name).replace('[playerDefence]', this.name);
+// 			el   = `<p> ${currentTime()} ${text} </p>`;
+// 			break;
+// 	}
+
+// 	$chat.insertAdjacentHTML('afterbegin', el);
+// }
 
 
